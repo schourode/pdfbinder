dd3d8cbcece7ed441e5b1b851b90e4a4d9a7ca73
schourode <jsr@malamute.dk>
2009-06-11 20:43:59 +0000

    Adding empty project scaffold.
    git-svn-id: http://pdfbinder.googlecode.com/svn/trunk@2 b5b26f92-56c3-11de-9010-b74abb91d602

---
+++ src/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PDFBinder")]
+[assembly: AssemblyDescription("A simple tool to merge several PDF documents into one")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Malamute.dk v/Joern Schou-Rode")]
+[assembly: AssemblyProduct("PDFBinder")]
+[assembly: AssemblyCopyright("Copyright © 2009 - Joern Schou-Rode")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c91bd3ad-1f6c-49c6-b674-bd2870ad4440")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
