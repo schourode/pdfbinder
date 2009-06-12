224a5dea734d468f26d8f43698ac8c58471437e8
schourode <jsr@malamute.dk>
2009-06-12 16:13:25 +0000

    Adds .ico version of logo, and assigns logo to MainForm.
    git-svn-id: http://pdfbinder.googlecode.com/svn/trunk@6 b5b26f92-56c3-11de-9010-b74abb91d602

--- src/MainForm.Designer.cs
+++ src/MainForm.Designer.cs
@@ -28,9 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "PDFBinder";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
