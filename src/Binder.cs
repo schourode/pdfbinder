212222de339414f19b5b1b328c0403e911e8c754
schourode <jsr@malamute.dk>
2010-01-22 11:12:28 +0000

    Switches to GPL because iTextSharp does not allow us to use MIT license.
    git-svn-id: http://pdfbinder.googlecode.com/svn/trunk@17 b5b26f92-56c3-11de-9010-b74abb91d602

--- src/Binder.cs
+++ src/Binder.cs
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2009, 2010 Joern Schou-Rode
+ * 
+ * This file is part of PDFBinder.
+ *
+ * PDFBinder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * PDFBinder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with PDFBinder.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 using System;
 using System.IO;
 using iTextSharp.text;
--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -1,36 +1,31 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿/*
+ * Copyright 2009, 2010 Joern Schou-Rode
+ * 
+ * This file is part of PDFBinder.
+ *
+ * PDFBinder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * PDFBinder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with PDFBinder.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("PDFBinder")]
-[assembly: AssemblyDescription("A simple tool to merge several PDF documents into one")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Malamute.dk v/Joern Schou-Rode")]
 [assembly: AssemblyProduct("PDFBinder")]
-[assembly: AssemblyCopyright("Copyright © 2009 - Joern Schou-Rode")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyDescription("A simple tool to merge several PDF documents into one")]
+[assembly: AssemblyCopyright("Copyright 2009, 2010 Joern Schou-Rode")]
+[assembly: AssemblyCompany("Malamute.dk")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c91bd3ad-1f6c-49c6-b674-bd2870ad4440")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.*")]
